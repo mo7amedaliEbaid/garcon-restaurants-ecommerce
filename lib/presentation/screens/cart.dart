f978e60800182dbf730ef1d09c245317586b5763
Mohamed Ali <mo7amedaliebaid@gmail.com>
2024-02-12 18:40:02 +0200

    cart

--- lib/presentation/screens/cart.dart
+++ lib/presentation/screens/cart.dart
@@ -2,6 +2,11 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:garcon/application/application.dart';
+import 'package:garcon/configs/app_dimensions.dart';
+import 'package:garcon/configs/app_typography.dart';
+import 'package:garcon/configs/space.dart';
+import 'package:garcon/core/core.dart';
+import 'package:garcon/presentation/widgets.dart';
 
 class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
@@ -20,18 +25,79 @@ class _CartScreenState extends State<CartScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: BlocBuilder<CartCubit, CartState>(
-          builder: (context, state) {
-            if (state is CartLoaded && state.pickUps.isNotEmpty) {
-              return Text(state.pickUps.first.description);
-            }else if (state is CartLoaded && state.pickUps.isEmpty) {
-              return Text("empty");
-            } else {
-              return Text("error");
-            }
-          },
-        ),
+      appBar: customAppBar(
+        context: context,
+        title: "Cart",
+      ),
+      body: Column(
+        children: [
+          Space.yf(2.5),
+          BlocBuilder<CartCubit, CartState>(
+            builder: (context, state) {
+              if (state is CartLoaded && state.pickUps.isNotEmpty) {
+                return Column(
+                  children: [
+                    SizedBox(
+                      height: AppDimensions.normalize(180),
+                      child: ListView.separated(
+                        itemCount: state.pickUps.length,
+                        itemBuilder: (context, index) {
+                          return pickupItem(
+                            context: context,
+                            pickUp: state.pickUps[index],
+                          );
+                        },
+                        separatorBuilder: (BuildContext context, int index) {
+                          return Space.yf();
+                        },
+                      ),
+                    ),
+                    Space.yf(.5),
+                    Container(
+                      width: double.infinity,
+                      height: AppDimensions.normalize(16),
+                      margin: Space.hf(2),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: AppColors.deepRed),
+                          borderRadius: BorderRadius.circular(
+                              AppDimensions.normalize(5))),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Icon(
+                            Icons.add_circle_outline_rounded,
+                            color: AppColors.deepRed,
+                          ),
+                          Space.yf(.3),
+                          Text(
+                            "Add More Meals",
+                            style:
+                                AppText.h3?.copyWith(color: AppColors.deepRed),
+                          )
+                        ],
+                      ),
+                    ),
+
+                  ],
+                );
+              } else if (state is CartLoaded && state.pickUps.isEmpty) {
+                return Text("empty");
+              } else {
+                return Text("error");
+              }
+            },
+          ),
+          Space.ym!,
+          customElevatedButton(
+              withArrow: true,
+              width: double.infinity,
+              height: AppDimensions.normalize(20),
+              color: AppColors.deepRed,
+              borderRadius: 0,
+              text: "Proceed to Checkout",
+              textStyle: AppText.h3b!.copyWith(color: Colors.white),
+              onPressed: () {})
+        ],
       ),
     );
   }
