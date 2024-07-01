cbeedbbea28ac46d4c9abd9de92290e58c030f83
Mohamed Ali <mo7amedaliebaid@gmail.com>
2024-07-01 06:32:50 +0300

    update

--- lib/presentation/screens/pickups_checkout.dart
+++ lib/presentation/screens/pickups_checkout.dart
@@ -58,146 +58,148 @@ class _PickupsCheckoutState extends State<PickupsCheckout> {
         context: context,
         title: "Checkout",
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: Space.hf(1.5),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Space.yf(1.5),
-                Text(
-                  widget.restaurant.name.capitalize(),
-                  style: AppText.h2b,
-                ),
-                Space.yf(.1),
-                Text(
-                  "(Order will be ready for pick-up in 45 minutes)",
-                  style: AppText.b2?.copyWith(color: AppColors.greyColor),
-                ),
-                Space.yf(1.5),
-                Text(
-                  "Customer Name",
-                  style: AppText.b1b,
-                ),
-                Space.yf(.5),
-                customTextField(
-                    labelText: "Type here", controller: _nameController),
-                Space.yf(1.5),
-                Text(
-                  "Restaurant Branch",
-                  style: AppText.b1b,
-                ),
-                Space.yf(.5),
-                Container(
-                  padding: Space.h1!,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey.withOpacity(.5)),
-                    borderRadius: BorderRadius.circular(
-                      AppDimensions.normalize(4),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: Space.hf(1.5),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Space.yf(1.5),
+                  Text(
+                    widget.restaurant.name.capitalize(),
+                    style: AppText.h2b,
+                  ),
+                  Space.yf(.1),
+                  Text(
+                    "(Order will be ready for pick-up in 45 minutes)",
+                    style: AppText.b2?.copyWith(color: AppColors.greyColor),
+                  ),
+                  Space.yf(1.5),
+                  Text(
+                    "Customer Name",
+                    style: AppText.b1b,
+                  ),
+                  Space.yf(.5),
+                  customTextField(
+                      labelText: "Type here", controller: _nameController),
+                  Space.yf(1.5),
+                  Text(
+                    "Restaurant Branch",
+                    style: AppText.b1b,
+                  ),
+                  Space.yf(.5),
+                  Container(
+                    padding: Space.h1!,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey.withOpacity(.5)),
+                      borderRadius: BorderRadius.circular(
+                        AppDimensions.normalize(4),
+                      ),
+                    ),
+                    child: DropdownButton(
+                        isExpanded: true,
+                        underline: const SizedBox.shrink(),
+                        value: dropDownValue,
+                        items: dropDownItems
+                            .map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        onChanged: (value) {
+                          setState(() {
+                            dropDownValue = value!;
+                          });
+                        }),
+                  ),
+                  Space.yf(1.5),
+                  Text(
+                    "Pickup Time",
+                    style: AppText.b1b,
+                  ),
+                  Space.yf(.5),
+                  Container(
+                    padding: Space.all(1.1, .7),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey.withOpacity(.5)),
+                      borderRadius: BorderRadius.circular(
+                        AppDimensions.normalize(4),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(selectedTime.format(context)),
+                        GestureDetector(
+                          onTap: () {
+                            _selectTime(context);
+                          },
+                          child: const Icon(
+                            Icons.timelapse,
+                            color: Colors.grey,
+                          ),
+                        )
+                      ],
                     ),
                   ),
-                  child: DropdownButton(
-                      isExpanded: true,
-                      underline: const SizedBox.shrink(),
-                      value: dropDownValue,
-                      items: dropDownItems
-                          .map<DropdownMenuItem<String>>((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        );
-                      }).toList(),
-                      onChanged: (value) {
-                        setState(() {
-                          dropDownValue = value!;
-                        });
-                      }),
-                ),
-                Space.yf(1.5),
-                Text(
-                  "Pickup Time",
-                  style: AppText.b1b,
-                ),
-                Space.yf(.5),
-                Container(
-                  padding: Space.all(1.1, .7),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey.withOpacity(.5)),
-                    borderRadius: BorderRadius.circular(
-                      AppDimensions.normalize(4),
-                    ),
+                  Space.yf(.4),
+                  Text(
+                    "For Today (${DateTime.now().toString().substring(0, 10)})",
+                    style: AppText.b2?.copyWith(color: AppColors.greyColor),
                   ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(selectedTime.format(context)),
-                      GestureDetector(
-                        onTap: () {
-                          _selectTime(context);
-                        },
-                        child: const Icon(
-                          Icons.timelapse,
-                          color: Colors.grey,
+                  Space.yf(2.5),
+                  Center(
+                    child: Column(
+                      children: [
+                        Text(
+                          "Total Amount",
+                          style: AppText.h3b,
                         ),
-                      )
-                    ],
+                        Space.yf(.2),
+                        Text(
+                          "${widget.amount} KWD",
+                          style: AppText.h1b?.copyWith(color: AppColors.deepRed),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                Space.yf(.4),
-                Text(
-                  "For Today (${DateTime.now().toString().substring(0, 10)})",
-                  style: AppText.b2?.copyWith(color: AppColors.greyColor),
-                ),
-                Space.yf(2.5),
-                Center(
-                  child: Column(
-                    children: [
-                      Text(
-                        "Total Amount",
-                        style: AppText.h3b,
-                      ),
-                      Space.yf(.2),
-                      Text(
-                        "${widget.amount} KWD",
-                        style: AppText.h1b?.copyWith(color: AppColors.deepRed),
-                      ),
-                    ],
-                  ),
-                ),
+                ],
+              ),
+            ),
+            Space.yf(5),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                customElevatedButton(
+                    width: AppDimensions.normalize(65),
+                    height: AppDimensions.normalize(20),
+                    color: AppColors.deepRed,
+                    borderRadius: AppDimensions.normalize(7),
+                    text:
+                        "Pay ${convertKwdToUsd(double.parse(widget.amount))} USD",
+                    textStyle: AppText.b1b!.copyWith(color: Colors.white),
+                    onPressed: () {
+                      //    Navigator.of(context).pushNamed(AppRouter.payment);
+                      context.read<PaymentCubit>().createPaymentIntent(
+                            amount:'${convertKwdToUsd(double.parse(widget.amount))}',
+                          );
+                    }),
+                customOutlinedButton(
+                    width: AppDimensions.normalize(65),
+                    height: AppDimensions.normalize(20),
+                    borderColor: AppColors.darkRed,
+                    borderRadius: AppDimensions.normalize(7),
+                    text: "Pay On Pickup",
+                    textStyle: AppText.b1b!.copyWith(color: AppColors.darkRed),
+                    onPressed: () {})
               ],
             ),
-          ),
-          Space.ym!,
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              customElevatedButton(
-                  width: AppDimensions.normalize(65),
-                  height: AppDimensions.normalize(20),
-                  color: AppColors.deepRed,
-                  borderRadius: AppDimensions.normalize(7),
-                  text:
-                      "Pay ${convertKwdToUsd(double.parse(widget.amount))} USD",
-                  textStyle: AppText.b1b!.copyWith(color: Colors.white),
-                  onPressed: () {
-                    //    Navigator.of(context).pushNamed(AppRouter.payment);
-                    context.read<PaymentCubit>().createPaymentIntent(
-                          amount:'${convertKwdToUsd(double.parse(widget.amount))}',
-                        );
-                  }),
-              customOutlinedButton(
-                  width: AppDimensions.normalize(65),
-                  height: AppDimensions.normalize(20),
-                  borderColor: AppColors.darkRed,
-                  borderRadius: AppDimensions.normalize(7),
-                  text: "Pay On Pickup",
-                  textStyle: AppText.b1b!.copyWith(color: AppColors.darkRed),
-                  onPressed: () {})
-            ],
-          ),
-          Space.yf()
-        ],
+            Space.yf()
+          ],
+        ),
       ),
     );
   }
